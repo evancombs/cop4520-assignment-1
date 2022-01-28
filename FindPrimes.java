3b8f6d1fbc2ea56ebd9f9cbee44028c165c6570f
Evan Combs <evan@combs-family.com>
2022-01-28 03:37:19 -0500

    Implemented conccurent prime sieve algorithm Added file output / writing Updated README

--- FindPrimes.java
+++ FindPrimes.java
@@ -1,10 +1,12 @@
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.Collections;
+// import java.util.concurrent.CopyOnWriteArrayList;
+// import java.util.Collections;
 import java.util.ArrayList;
 import java.util.*;
+import java.io.*;
+
 // The handler class for our multithreaded prime-finding solution
 class FindPrimes
 {
@@ -23,11 +25,12 @@ class FindPrimes
       System.out.println("Error -- Run with \"java FindPrimes.java <number of threads> <number to search to>\"");
       return;
     }
-    FindPrimesSequential(max);
+    // FindPrimesSequential(max);
+    // FindPrimesSequentialSieve(max);
     FindPrimesConcurrent(numThreads, max);
   }
 
-  // Returns all the prime numbers up to n
+  // Returns all the prime numbers up to n in a bruteforce method
   public static void FindPrimesSequential(int max)
   {
     System.out.println("Finding all primes up to " + max + " sequentially! This may take some time...");
@@ -35,6 +38,8 @@ class FindPrimes
 
     long sumOfPrimes = 0;
     int primesFound = 0;
+
+    // We simply check every possible number, to see if it is prime.
     for (int i = 0; i < max; i++)
     {
       if (isPrime(i))
@@ -49,22 +54,59 @@ class FindPrimes
     System.out.println("Sum of primes: " + sumOfPrimes);
   }
 
+  // Returns all prime numbers up to max using a prime sieve
+  public static void FindPrimesSequentialSieve(int max)
+  {
+    boolean[] isPrime = new boolean[max];
+    long sumOfPrimes = 0;
+    int primesFound = 0;
+
+    System.out.println("Finding all primes up to " + max + " sequentially, with a SIEVE! This may take some time...");
+    long startTime = System.currentTimeMillis();
+
+    // Initialize prime array
+    for (int i = 0; i < max; i++)
+      isPrime[i] = true;
+
+    for (int i = 2; i < max; i++)
+    {
+      if (isPrime[i])
+      {
+        sumOfPrimes += i;
+        primesFound++;
+        for (int j = i; j < max; j += i)
+        {
+            isPrime[j] = false;
+        }
+      }
+    }
+
+    long endTime = System.currentTimeMillis();
+    System.out.println("Execution time: " + (endTime - startTime));
+    System.out.println("Primes found: " + primesFound);
+    System.out.println("Sum of primes: " + sumOfPrimes);
+  }
+
+  // Returns all prime numbers up to max, using numThreads threads.
   public static void FindPrimesConcurrent(int numThreads, int max)
   {
-    System.out.println("Finding all primes up to " + max + " sequentially! This may take some time...");
+    System.out.println("Finding all primes up to " + max + " concurrently! This may take some time...");
     long startTime = System.currentTimeMillis();
 
     ArrayList<Thread> threads = new ArrayList<>();
 
-    // Create a new threadRunner with the desire number to find primes up to
+    // Create a new threadRunner with the desired number to find primes up to
     ThreadRunner threadRunner = new ThreadRunner((int) max);
     for (int i = 0; i < numThreads; i++)
     {
       Thread thread = new Thread(threadRunner);
       threads.add(thread);
-      thread.start();
     }
-    // Join threads, so that the program waits until they have all completed
+
+    for (Thread thread : threads)
+        thread.start();
+
+    // Join threads, so that the execution waits until they have all completed
     try
     {
       for (Thread thread : threads)
@@ -72,14 +114,19 @@ class FindPrimes
     }
     catch (Exception e)
     {
-      System.out.println("Caught an exception!");
+      System.out.println("Failed joining threads!");
     }
 
     long endTime = System.currentTimeMillis();
-    System.out.println("Execution time: " + (endTime - startTime));
-    System.out.println("Primes found: " + threadRunner.GetPrimesFound());
-    System.out.println("Sum of primes: " + threadRunner.GetSumOfPrimes());
-    threadRunner.PrintTopTenPrimes();
+    threadRunner.countSieve();
+
+    // System.out.println("Execution time: " + (endTime - startTime));
+    // System.out.println("Primes found: " + threadRunner.GetPrimesFound());
+    // System.out.println("Sum of primes: " + threadRunner.GetSumOfPrimes());
+    // threadRunner.PrintTopTenPrimes();
+
+    WriteResultsToFile("output.txt", endTime - startTime, threadRunner.GetSumOfPrimes(),
+                        threadRunner.GetPrimesFound(), threadRunner.getTopTenPrimes());
   }
   // A naive but simple method to check if a number is prime, by checking all
   // numbers up to it.
@@ -97,20 +144,50 @@ class FindPrimes
     }
     return true;
   }
+  private static void WriteResultsToFile(String filename, long executionTime, long sumOfPrimes, int primesFound, int[] topTen)
+  {
+    try
+    {
+      File file = new File(filename);
+      PrintWriter writer = new PrintWriter(file);
+      // writer.println("Test");
+      writer.println("Execution time: " + executionTime);
+      writer.println("Number of primes found: " + primesFound);
+      writer.println("Sum of all primes: " + sumOfPrimes);
+
+      writer.println("\nTop ten maximum primes");
+
+      Arrays.sort(topTen);
+      //System.out.println("Top ten largest primes found:");
+      for (int i = 0; i < topTen.length; i++)
+      {
+        //System.out.println(topTenPrimes[i]);
+        writer.println(topTen[i]);
+      }
+      writer.close();
+    }
+    catch(Exception e)
+    {
+      System.out.println("Failed to write output file!");
+    }
+  }
 }
 
 
 class ThreadRunner implements Runnable
 {
   // Thread control members
-  static AtomicInteger counter = new AtomicInteger(1);
+  static AtomicInteger counter = new AtomicInteger(2);
   AtomicBoolean endFlag;
   public int n;
+
+  AtomicBoolean[] isPrime;
 
   // Execution results
   public AtomicInteger primesFound;
   public AtomicLong sumOfPrimes;
 
+  // Holds the last 10
   int[] topTenPrimes;
 
   public ThreadRunner(int numberUpTo)
@@ -120,22 +197,65 @@ class ThreadRunner implements Runnable
     sumOfPrimes = new AtomicLong(0);
     endFlag = new AtomicBoolean(false);
 
+    isPrime = new AtomicBoolean[numberUpTo];
+
+    // Initialize sieve array
+    for (int i = 0; i < numberUpTo; i++)
+    {
+        isPrime[i] = new AtomicBoolean(true);
+    }
+
     topTenPrimes = new int[10];
   }
   public void run()
   {
     int current = 0;
 
-    while (endFlag.get() && (current = counter.getAndIncrement()) <= n)
+
+    while((current = counter.getAndIncrement()) < Math.sqrt(n))
     {
-      if (FindPrimes.isPrime(current))
+      if(isPrime[current].get())
       {
-        topTenPrimes[primesFound.get() % 10] = current;
-        primesFound.getAndIncrement();
-        sumOfPrimes.addAndGet(current);
+        // System.out.println(current + " is prime!");
+        // Fill out the sieve for this value, starting at the next multiple
+        // Ex: 5 is the current value. We want to count 5 later, so we start at
+        // 25, and mark 25, 30, 35,...
+        for (int i = current * current; i < n; i += current)
+          isPrime[i].set(false);
       }
     }
-    endFlag.set(true);
+  }
+
+  public void sieve()
+  {
+    int current = 0;
+    while((current = counter.getAndIncrement()) < Math.sqrt(n))
+    {
+      if(isPrime[current].get())
+      {
+        // primesFound.getAndIncrement();
+        // sumOfPrimes.addAndGet(current);
+        // topTenPrimes[primesFound.get() % 10] = current;
+
+        // Fill out the sieve for this value, starting at the next multiple
+        for (int i = current * current; i < n; i += current)
+          isPrime[i].set(false);
+        //for (int i = current * current)
+      }
+    }
+  }
+
+  public void countSieve()
+  {
+    for (int i = 2; i < n; i++)
+    {
+      if(isPrime[i].get())
+      {
+        primesFound.getAndIncrement();
+        sumOfPrimes.addAndGet(i);
+        topTenPrimes[primesFound.get() % 10] = i;
+      }
+    }
   }
   public int GetPrimesFound()
   {
@@ -145,6 +265,11 @@ class ThreadRunner implements Runnable
   public long GetSumOfPrimes()
   {
     return sumOfPrimes.get();
+  }
+
+  public int[] getTopTenPrimes()
+  {
+    return topTenPrimes;
   }
 
   public void PrintTopTenPrimes()
--- README.txt
+++ README.txt
@@ -2,8 +2,11 @@ Compile with "javac FindPrimes.java"
 Compilation generates FindPrimes.class and ThreadRunner.class
 
 Input
-	Run with "java FindPrimes <number of threads> <number to find primes up to>
+	Run with "java FindPrimes <number of threads> <number to find primes up to>"
 	
+Output
+	Outputs to file "output.txt" with following format:
+	<
 Experimental Evaluation
 	Empirical analysis is performed using the System.currentTimeMillis() method. Threads are joined to ensure that program
 	main execution pauses until all concurrent threads have completed their tasks. Execution time is compared between a
@@ -14,11 +17,28 @@ Experimental Evaluation
 		10^4	4ms						6ms
 		10^5	20ms					18ms
 		10^6	399ms					124ms
-		10^7	10,578ms				2388ms
-		10^8	293,861ms				6,1255ms
+		10^7	10,578ms				1,063ms
+		10^8	293,861ms				11,186ms
+		
+		n		sequential time 		concurrent time 		sequential / n			concurrent / n
+		10^2	0ms						4ms						0
+		10^4	1ms						5ms						0.001
+		10^5	5ms						17ms					0.0005
+		10^6	17ms					93ms					0.000017				0.000093
+		10^7	124ms					103ms					0.0000124				0.0000103
+		10^8	1514ms					6921ms					0.00001514				0.00006921
 		
 	While a sequential solution is preferable for small values of n(likely due to an increased overhead and complexity
 	of the concurrent solution), it quickly becomes inefficient for large n.
 	
 Proof / Explanation
-	My solution uses
+	My solution implements the sieve of eratosthenes in a concurrent context. An array of atomic booleans
+	replace the typical sieve boolean array, and each number is assigned to a thread from an atomic counter.
+	
+	It may be expected that ocassionally, threads will wait on one another; for instance, if numbers 3 and 5 were
+	being handled by separate threads, one would have to wait for the other to set 15 as false, as both would need to.
+	This may be considered repeated computation, and there is likely a maximum number of threads before the overhead
+	exceed the benefits.
+	
+	The solution benefits from concurrency as each thread can handle marking the sieve for a different prime number.
+	Aside from cases such as mentioned above, more threads allow more primes to be marked from the sieve concurrently.
